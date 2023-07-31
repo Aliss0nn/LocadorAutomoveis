6b47ecac76b510e5500fb3aea2486bd91f84711f
Alisson  Scopel <alli_scopel@hotmail.com>
2023-07-31 18:02:09 -0300

    implementando banco modulo taxas e serviços

---
+++ LocadorVeiculos.Dominio/ModuloTaxasEServicos/TaxasEServico.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadorAutomoveis.Dominio.ModuloTaxasEServicos
+{
+    public class TaxasEServico : EntidadeBase<TaxasEServico>
+    {
+        public string Nome { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public EnumPlanoDeCalculo planoDeCalculo { get; set; }
+
+        public TaxasEServico()
+        {
+
+        }
+
+        public TaxasEServico(string nome, decimal preco, EnumPlanoDeCalculo planoDeCalculo)
+        {
+            Nome = nome;
+            Preco = preco;
+            this.planoDeCalculo = planoDeCalculo;
+        }
+
+        public TaxasEServico(int id, string nome,decimal preco,EnumPlanoDeCalculo planoDeCalculo) : this(nome,preco,planoDeCalculo)
+        {
+            Id = id;
+        }
+
+        public override void Atualizar(TaxasEServico registro)
+        {
+            this.Nome = registro.Nome;
+            this.Preco = registro.Preco;
+            this.planoDeCalculo = registro.planoDeCalculo;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is TaxasEServico servico &&
+                   Id == servico.Id &&
+                   Nome == servico.Nome &&
+                   Preco == servico.Preco &&
+                   planoDeCalculo == servico.planoDeCalculo;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Nome, Preco, planoDeCalculo);
+        }
+
+        
+    }
+}
---
+++ LocadorVeiculos.Infra.Orm/ModuloTaxasEServicos/RepositorioTaxasEServicosOrm.cs
@@ -0,0 +1,21 @@
+﻿using LocadorAutomoveis.Dominio.ModuloTaxasEServicos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadorAutomoveis.Infra.Orm.ModuloTaxasEServicos
+{
+    public class RepositorioTaxasEServicosOrm : RepositorioBaseEmOrm<TaxasEServico>, IRepositorioTaxasServico
+    {
+        public RepositorioTaxasEServicosOrm(LocadorAutomoveisDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public TaxasEServico SelecionarPorNome(string nome)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
